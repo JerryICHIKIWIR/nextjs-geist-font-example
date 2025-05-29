92cf3dbd4c676ae70544f00cf660f7771e176491
JerryICHIKIWIR <jerrysquest414@gmail.com>
2025-05-30 01:47:11 +0800

    Add .next/static/chunks/src_app_news_page_tsx_6cb64fe7._.js 1748540830563

---
+++ .next/static/chunks/src_app_news_page_tsx_6cb64fe7._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d19e6e9a._.js",
+  "static/chunks/src_34e571be._.js"
+],
+    source: "dynamic"
+});
