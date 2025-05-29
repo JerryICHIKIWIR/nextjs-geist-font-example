c72cf917212b766f8b2688ed3bb310fc2a08ca5e
JerryICHIKIWIR <jerrysquest414@gmail.com>
2025-05-30 01:48:18 +0800

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1748540897487

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_58dd17f0._.js",
+  "static/chunks/src_c0423d1f._.js"
+],
+    source: "dynamic"
+});
